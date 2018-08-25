c426f28b430ef51adb1b0318c63d2b04899125ff
Kevin Homuth <kevin.homuth@haw-hamburg.de>
2018-08-25 10:16:40 +0200

    basic navbar and created css

--- index.php
+++ index.php
@@ -5,15 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Testsite</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--<link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>-->
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <!--<script src="main.js"></script>-->
 </head>
 <body>
     <header>
         <div class="nav">
             <a href="">Home</a>
+            <div class="dropdown">
+                <a class="droptrigger">Produkte</a>
+                <div class="dropcontent">
+                    <table>
+                        <tr>
+                            <td>Alle Produkte</td>
+                            <td>
+                                <ul>
+                                    <li>Produkt 1</li>
+                                    <li>Produkt 2</li>
+                                    <li>Produkt 3</li>
+                                </ul>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </div>
     </header>
-    <footer></footer>
+    <footer>
+        <a href="">Impressum</a>
+        <a href="">AGB</a>
+        <a href="">Datenschutz</a>
+    </footer>
 </body>
 </html>
